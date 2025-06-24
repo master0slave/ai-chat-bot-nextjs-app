ff92760cbd7628d2641dc32d574181d4b4432fa3
Nonthiwat <nonthiwat.sct@gmail.com>
2025-06-24 21:35:55 +0700

    Update greeting message in ChatPage to use Thai language

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -14,7 +14,7 @@ export default async function ChatPage() {
   return (
     <div className="flex h-svh w-full items-center justify-center gap-2">
       <p>
-        Hello <span>{data.user.email} ID: (data.user.id)</span>
+        สวัสดี <span>{data.user.email} ID: {data.user.id}</span>
       </p>
       <LogoutButton />
     </div>
